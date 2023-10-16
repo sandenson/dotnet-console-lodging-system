8e1507f2e6ab5d70c9cc08fe66a0a5ec44208b20
sandenson <lgabrielsandes@gmail.com>
2023-10-16 12:09:26 -0300

    feat: Objective completed.

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException(
+                    $"Não é possível reservar a suíte para {hospedes.Count} hóspedes, "
+                    + $"pois a suíte só tem a capacidade de receber {Suite.Capacidade} hóspedes."
+                );
             }
         }
 
@@ -35,23 +35,16 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                return valor * 0.9M;
             }
 
             return valor;
--- Program.cs
+++ Program.cs
@@ -3,23 +3,70 @@ using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+static void RunTest(int opcao)
+{
+    int numeroDeHospedes = opcao == 3 ? 4 : 2;
+    int capacidade = 3;
+    int diasReservados = opcao == 2 ? 10 : 5;
+    
+    // Cria os modelos de hóspedes e cadastra na lista de hóspedes
+    List<Pessoa> hospedes = new();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+    for (int i = 0; i < numeroDeHospedes; i++)
+    {
+        hospedes.Add(new(nome: $"Hóspede {i + 1}"));
+    }
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+    // Cria a suíte
+    Suite suite = new(tipoSuite: "Premium", capacidade, valorDiaria: 30);
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+    // Cria uma nova reserva, passando a suíte e os hóspedes
+    Reserva reserva = new(diasReservados);
+    reserva.CadastrarSuite(suite);
+    reserva.CadastrarHospedes(hospedes);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+    // Exibe a quantidade de hóspedes e o valor da diária
+    Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+    Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+}
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("Olá. Escolha uma da opções de teste:");
+    Console.WriteLine("1 - Teste de sucesso");
+    Console.WriteLine("2 - Teste de sucesso (desconto)");
+    Console.WriteLine("3 - Teste de falha (mais hóspedes que possível)");
+    Console.WriteLine("4 - Sair");
+    Console.WriteLine("Digite um dos números e aperte Enter para escolher uma opção.");
+
+    int opcao = 0;
+
+    while (true)
+    {
+        bool valido = int.TryParse(Console.ReadLine(), out opcao) && opcao >= 1 && opcao <= 4;
+
+        if (valido) break;
+
+        Console.WriteLine("Opção inválida! Tente novamente.");
+    }
+
+    if (opcao == 4) break;
+
+    Console.Clear();
+    
+    try
+    {
+        RunTest(opcao);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+    }
+
+    Console.WriteLine("Aperte qualquer tecla para voltar ao menu.");
+    Console.ReadKey();
+}
+
+Console.WriteLine("Até mais.");
